12079aed47d86efdfbeb82b5bc567be37a0e89c5
Cosmin Tudose <56267750+cosmintudose@users.noreply.github.com>
2025-08-14 15:34:26 +0200

    added encode chip seq data links

--- 0_setup.sh
+++ 0_setup.sh
@@ -18,14 +18,14 @@ wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM6893nnn/GSM6893214/suppl/GSM689
 wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM6893nnn/GSM6893214/suppl/GSM6893214%5FH3K27Tri%2D9733S%5FInput%2Dq05%5Fpeaks.broadPeak.gz --directory-prefix=./publicly_available_data
 
 #HL60 chip-seq #####################################not good
-wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5330nnn/GSM5330834/suppl/GSM5330834%5FENCFF598BHR%5Fpseudoreplicated%5Fpeaks%5FGRCh38.bigBed --directory-prefix=./publicly_available_data
+#me3
+wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5330nnn/GSM5330834/suppl/GSM5330834%5FENCFF432FAX%5Fpseudoreplicated%5Fpeaks%5FGRCh38%2Ebed%2Egz --directory-prefix=./publicly_available_data
+
+#ac
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE167nnn/GSE167788/suppl/GSE167788%5FENCFF763UAG%5Freplicated%5Fpeaks%5FGRCh38%2Ebed%2Egz --directory-prefix=./publicly_available_data
 
 #K562 Micro-C - this is 7.6 GB
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE206nnn/GSE206131/suppl/GSE206131%5FK562.mapq%5F30.mcool --directory-prefix=./publicly_available_data
-
-#TARGET AML data
-wget https://cbioportal-datahub.s3.amazonaws.com/aml_target_2018_pub.tar.gz --directory-prefix=./publicly_available_data
-tar -xvzf ./publicly_available_data/aml_target_2018_pub.tar.gz -C ./publicly_available_data
 
 #blacklist genomic regions
 wget https://raw.githubusercontent.com/Boyle-Lab/Blacklist/master/lists/hg38-blacklist.v2.bed.gz --directory-prefix=./genome
